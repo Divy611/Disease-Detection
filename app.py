2174b0e83c5477fc784f002ca3265c0689ba0e3d
Divy611 <divy.parikh@hotmail.com>
2025-02-16 17:15:17 +0530

    App modified

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import streamlit as st
 from keras.models import load_model
 
 model = load_model('disease_prediction_model.h5')
+
 pickle_folder = 'pickle_files'
 
 disease_encoder_path = os.path.join(pickle_folder, 'le_disease.pkl')
@@ -15,8 +16,10 @@ if not os.path.exists(disease_encoder_path):
 with open(disease_encoder_path, 'rb') as f:
     le_disease = pickle.load(f)
 
-symptom_columns = ['Symptom_1', 'Symptom_2',
-                   'Symptom_3', 'Symptom_4', 'Symptom_5']
+symptom_columns = ['Symptom_1', 'Symptom_2', 'Symptom_3', 'Symptom_4', 'Symptom_5',
+                   'Symptom_6', 'Symptom_7', 'Symptom_8', 'Symptom_9', 'Symptom_10',
+                   'Symptom_11', 'Symptom_12', 'Symptom_13', 'Symptom_14', 'Symptom_15',
+                   'Symptom_16', 'Symptom_17']
 label_encoders = {}
 for column in symptom_columns:
     label_encoder_path = os.path.join(
@@ -30,21 +33,23 @@ for column in symptom_columns:
 st.title("Disease Prediction App")
 
 sym_count = st.number_input(
-    'How many symptoms are you facing?', min_value=1, step=1)
+    'How many symptoms are you facing?', min_value=1, max_value=17, step=1)
 
 user_symptoms = []
 for i in range(sym_count):
     symptom_choice = st.selectbox(f"Enter symptom {i+1}:",
                                   options=label_encoders[symptom_columns[0]].classes_,
                                   key=f'symptom_{i}')
-
     encoded_symptom = label_encoders[symptom_columns[0]].transform([symptom_choice])[
         0]
     user_symptoms.append(encoded_symptom)
 
-while len(user_symptoms) < len(symptom_columns):
+while len(user_symptoms) < 17:
     user_symptoms.append(0)
+
+user_symptoms_np = np.array(user_symptoms).reshape(1, -1)
+
 if st.button("Predict"):
-    prediction = model.predict([user_symptoms])
+    prediction = model.predict(user_symptoms_np)
     predicted_disease = le_disease.inverse_transform([prediction.argmax()])
     st.write(f"Predicted Disease: {predicted_disease[0]}")
